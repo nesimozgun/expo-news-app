0425ffbb4f1a1aabbe345cddf3f9b2853bc9b83c
nesimozgun <ozgunnesim@gmail.com>
2024-10-16 23:11:26 +0300

    add Breaking News to Home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import Header from '@/components/Header'
 import SearchBar from '@/components/SearchBar'
+import BreakingNews from '@/components/BreakingNews'
 import { NewsDataType } from "@/types"
-import axios from "axios"
-
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import axios from "axios"
 
 type Props = {}
 
 const Page = (props: Props) => {
   const {top: safeTop} = useSafeAreaInsets()
   const [breakingNews, setBreakingNews] = useState<NewsDataType[]>([])
+  const [isLoading, setIsLoading] = useState(true)
   const getBreakingNews = async () => {
     try {
       const url = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=br,us&language=en,pt&removeduplicate=1&size=5`
@@ -19,19 +20,27 @@ const Page = (props: Props) => {
 
       if(response && response.data) {
         setBreakingNews(response.data.results)
+        setIsLoading(false)
       }
     } catch (error) {
       console.log(error);
     }
   }
 
+  useEffect(() => {
+    getBreakingNews();
+  }, []);
+
   return (
     <View style={[styles.container, {paddingTop: safeTop}]}>
       <Header />
       <SearchBar />
-      {breakingNews.map((item, index) => (
-        <Text>{item.title}</Text>
-      ))}
+      {isLoading ? (
+        <ActivityIndicator size={'large'} />
+      ) : (
+        <BreakingNews newsList={breakingNews} />
+      )}
+      
     </View>
   )
 }
