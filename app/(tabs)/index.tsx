532fc05bf9fffa9f702b77b3ffee1e860115d4bc
nesimozgun <ozgunnesim@gmail.com>
2024-10-16 18:01:58 +0300

    add Header and SearchBar to Home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,37 @@
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import Header from '@/components/Header'
+import SearchBar from '@/components/SearchBar'
+import { NewsDataType } from "@/types"
+import axios from "axios"
+
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 type Props = {}
 
 const Page = (props: Props) => {
+  const {top: safeTop} = useSafeAreaInsets()
+  const [breakingNews, setBreakingNews] = useState<NewsDataType[]>([])
+  const getBreakingNews = async () => {
+    try {
+      const url = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=br,us&language=en,pt&removeduplicate=1&size=5`
+      const response = await axios.get(url)
+
+      if(response && response.data) {
+        setBreakingNews(response.data.results)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
+    <View style={[styles.container, {paddingTop: safeTop}]}>
+      <Header />
+      <SearchBar />
+      {breakingNews.map((item, index) => (
+        <Text>{item.title}</Text>
+      ))}
     </View>
   )
 }
@@ -16,7 +41,5 @@ export default Page
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
 })
